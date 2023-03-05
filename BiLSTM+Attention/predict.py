445e1ece72092255741c2d26cd686466400e8224
Qiming Bao <qiming.bao@xtracta.com>
2023-03-05 00:14:28 +0000

    update test script

---
+++ BiLSTM+Attention/predict.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+import tensorflow as tf
+
+from util import make_w2v_embeddings
+from util import split_and_zero_padding
+from util import ManDist
+
+# File paths
+TEST_CSV = '../Data/Model_train_dev_test_dataset/Other_model_train_dev_test_dataset/test-20.csv'
+
+# Load training set
+test_df = pd.read_csv(TEST_CSV)
+for q in ['question1', 'question2']:
+    test_df[q + '_n'] = test_df[q]
+
+# Make word2vec embeddings
+embedding_dim = 300
+max_seq_length = 20
+test_df, embeddings = make_w2v_embeddings(test_df, embedding_dim=embedding_dim, empty_w2v=False)
+
+# Split to dicts and append zero padding.
+X_test = split_and_zero_padding(test_df, max_seq_length)
+
+# Make sure everything is ok
+assert X_test['left'].shape == X_test['right'].shape
+
+# --
+
+model = tf.keras.models.load_model('./SiameseLSTM.h5', custom_objects={'ManDist': ManDist})
+model.summary()
+
+prediction = model.predict([X_test['left'], X_test['right']])
+print(prediction)
